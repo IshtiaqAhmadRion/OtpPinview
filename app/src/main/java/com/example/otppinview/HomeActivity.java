5da4993ba247d71633d6bbcb204e9e1e0487dac9
IshtiaqAhmadRion <43097671+IshtiaqAhmadRion@users.noreply.github.com>
2020-02-20 13:35:39 +0600

    abdulllah

--- app/src/main/java/com/example/otppinview/HomeActivity.java
+++ app/src/main/java/com/example/otppinview/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.example.otppinview;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -8,9 +9,11 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.google.firebase.auth.PhoneAuthCredential;
@@ -19,18 +22,36 @@ public class HomeActivity extends AppCompatActivity {
 
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle toggle;
+    ImageButton nav_button;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+
+        nav_button=findViewById(R.id.navigation_image);
+        nav_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(drawerLayout.isDrawerOpen(Gravity.RIGHT))
+                    drawerLayout.closeDrawer(Gravity.LEFT);
+
+                else
+                    drawerLayout.openDrawer(Gravity.LEFT);
+            }
+        });
+
         drawerLayout = findViewById(R.id.drawer_layout);
-        toggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
+        toggle = new ActionBarDrawerToggle(HomeActivity.this,drawerLayout,R.string.open,R.string.close);
+
 
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 
     }
 
